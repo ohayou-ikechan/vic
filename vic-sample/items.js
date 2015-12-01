bff91143c0347459ff3afd9840b0a63e7f18d3cf
Daisuke Ikeda <ohayou.ikechan@gmail.com>
2015-12-01 23:59:30 +0900

    to gitlab

--- vic-sample/items.js
+++ vic-sample/items.js
@@ -1,5 +1,7 @@
-function loadItems(){
-	return [
+// function loadItems(i){
+var items = [
+	//サンプルビデオ 1
+	[
 		{
 			itemName: "item1",
 			top: 190,
@@ -8,15 +10,18 @@ function loadItems(){
 			height: 100,
 			startTime: 0.5,
 			url: "http://amzn.to/1Si7qIT",
-			tag: "観葉植物",
-			animation:{
-			  x: "-170px",
-			  duration: 3000,
-			  easing: 'in',
-			  complete: function() {
-			  	this.remove();
-			  	this.added = false;
-			  }
+			translateX: -160,
+			translateY: 0,
+			duration: 3,
+			tag:{
+				text:"プラントポット<br>¥5600",
+				css:{
+					"font-family": "Sans-serif",
+					color: "white",
+					position: "absolute",
+					bottom: "-50px",
+					right: "-50px"
+				}
 			}
 		},
 		{
@@ -25,17 +30,21 @@ function loadItems(){
 			left: 0,
 			width: 165,
 			height: 160,
-			startTime: 3.7,
+			startTime: 4,
 			url: "http://amzn.to/1Si7qIT",
-			tag: "観葉植物",
-			animation:{
-			  x: "250px",
-			  duration: 3000,
-			  easing: 'in',
-			  complete: function() {
-			  	this.remove();
-			  	this.added = false;
-			  }
+			translateX: 250,
+			translateY: 0,
+			duration: 3,
+			tag:{
+				text:"プラントポット<br>¥5600",
+				css:{
+					"font-family": "Sans-serif",
+					color: "blue",
+					"font-size":"20px",
+					position: "absolute",
+					bottom: "-20px",
+					right: "-100px"
+				}
 			}
 		},
 		{
@@ -46,16 +55,134 @@ function loadItems(){
 			height: 315,
 			startTime: 7,
 			url: "http://amzn.to/1NLQM2D",
-			tag: "本棚",
-			animation:{
-			  x: "0px",
-			  duration: 3000,
-			  easing: 'in',
-			  complete: function() {
-			  	this.remove();
-			  	this.added = false;
-			  }
+			translateX: 0,
+			translateY: 0,
+			duration: 3,
+			tag:{
+				text:"ブックシェルフ<br>¥9800",
+				css:{
+					"font-family": "Sans-serif",
+					color: "tomato",
+					"font-size":"25px",
+					position: "absolute",
+					top: "10px"
+				}
 			}
 		}
-	];
-}
+	],
+	//サンプルビデオ 2
+	[
+		{
+			itemName: "sneaker-1",
+			top: 180,
+			left: 150,
+			width: 240,
+			height: 120,
+			startTime: 0,
+			url: "http://amzn.to/1Qu5der",
+			translateX: 0,
+			translateY: 0,
+			duration: 3,				
+			tag:{
+				text:"ナイキスニーカー<br>¥15000",
+				css:{
+					"font-family": "Sans-serif",
+					color: "white",
+					"font-size":"15px",
+					position: "absolute",
+					bottom: "-10px"
+				}
+			}
+		},			
+		{
+			itemName: "t-shirt-1",
+			top: 40,
+			left: 135,
+			width: 160,
+			height: 250,
+			startTime: 3.8,
+			url: "http://amzn.to/1ShM4KS",
+			translateX: -130,
+			translateY: 0,
+			duration: 1,
+			tag:{
+				text:"ナイキTシャツ<br>¥15000",
+				css:{
+					"font-family": "Sans-serif",
+					color: "pink",
+					"font-size":"15px",
+					position: "absolute",
+					bottom: "-10px"
+				}
+			}
+		},			
+		{
+			itemName: "t-shirt-2",
+			top: 40,
+			left: 180,
+			width: 110,
+			height: 200,
+			startTime: 4.8,
+			url: "http://amzn.to/1ShM4KS",
+			translateX: 300,
+			translateY: 0,
+			duration: 2,
+			tag:{
+				text:"ナイキTシャツ<br>¥15000",
+				css:{
+					"font-family": "Sans-serif",
+					color: "pink",
+					"font-size":"15px",
+					position: "absolute",
+					bottom: "-10px"
+				}
+			}
+		},
+		{
+			itemName: "sneaker-2",
+			top: 180,
+			left: 320,
+			width: 170,
+			height: 130,
+			startTime: 8,
+			url: "http://amzn.to/1Qu5der",
+			translateX: 0,
+			translateY: 150,
+			duration: 2,
+			tag:{
+				text:"ナイキスニーカー<br>¥15000",
+				css:{
+					"font-family": "Sans-serif",
+					color: "pink",
+					"font-size":"15px",
+					position: "absolute",
+					top: "-20px"
+				}
+			}
+		},
+		{
+			itemName: "t-shirt-3",
+			top: 0,
+			left: 355,
+			width: 170,
+			height: 133,
+			startTime: 13,
+			url: "http://amzn.to/1ShM4KS",
+			translateX: 0,
+			translateY: 200,
+			duration: 4,
+			tag:{
+				text:"ナイキTシャツ<br>¥15000",
+				css:{
+					"font-family": "Sans-serif",
+					color: "white",
+					"font-size":"15px",
+					position: "absolute",
+					bottom: "-10px"
+				}
+			}
+		}	
+	]
+];
+// 	return items[i];
+// }
