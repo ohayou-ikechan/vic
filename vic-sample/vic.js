84489eb2ed0f18284fe136f110e1413272858ded
junzomatunoo <junzo_m@wire-frames.net>
2015-12-10 21:27:15 +0900

    ##index.html 要素を調整
    ##index.css
    スタイルを少しだけ調整
    
    ##vic.js
    
    とりあえず一旦オブジェクト化
    モーダルの雛形を作成
    
    動作不具合まだあり

--- vic-sample/vic.js
+++ vic-sample/vic.js
@@ -1,76 +1,102 @@
 "use strict";
 
 var fps = 40;
-var vics =[];
 
-function Vic(id,items,videoObj){
+function Vic (items, $elem) {
+
 	this.items = items;
-	this.videoObj = videoObj;
-	this.vid = videoObj.get(0);
+  this.$el = $elem;
+	this.$video = this.$el.find('video');
+  this.$video_wrap = this.$video.parent();
+	this.video = this.$video[0];
 	this.pauseFlag = false;
-	this.vicId = id;
+
+  this.init();
+
+  return this;
+
 }
 
 Vic.prototype.play = function(){
-	this.itemObjs = createItemObjects(this.vicId,this.items,this.videoObj);
-	this.vid.play();
+
+  this.createItemObjects();
+	this.video.play();
 	this.pointerPlay_();
-}
+
+};
 
 Vic.prototype.pause = function(){
-	this.vid.pause();
-	this.pauseFlag = true;
-}
 
+  this.video.pause();
+	this.pauseFlag = true;
+
+};
 
 Vic.prototype.restart = function(){
-	this.vid.play();
+
+	this.play();
 	this.pauseFlag = false;
-}
+
+};
 
 Vic.prototype.pointerPlay_ = function(){
-	var me = this;
-	var vid = this.vid;
+
+  var me = this;
+	var vid = this.video;
+  var items = this.items;
+
 	doRender();
 
 	function doRender(){
+
 		setTimeout(function(){
 			if(!vid.ended){
+
 				if(!me.pauseFlag){
-					for(var itemObj of me.itemObjs){
+
+					for(var item of items){
+
+             var itemObj = item.$dom;
+
 						if(itemObj.data("startTime") < vid.currentTime && !itemObj.data("added")){
+
 							itemObj.data("added",true);
-							putItem(itemObj,me.videoObj.parent());
+							me.putItem(itemObj);
+
 						}else if(itemObj.data("endTime") < vid.currentTime && itemObj.data("added")){
+
 							itemObj.stop();
 							itemObj.css("display","none");
+
 						}else{
-							moveItem(itemObj);
+
+							me.moveItem(itemObj);
+
 						}
-					};
+					}
 				}
 				doRender();
 			}else{
+
 				$("#timeDisplay").text("");
+
 			}
 		}, 1000/fps);
 	}
-}
+};
 
-function initializeVic(){
-	for(var i=0; items.length > i; i++){
-		vics[i] = new Vic(i,items[i],$("#video-" + i));
-	}
-}
 
-function createItemObjects(id,items,videoObj){
-	//set valuables
-	var itemObjects = [];
-	for(var item of items){
+
+Vic.prototype.createItemObjects = function () {
+
+    var that = this;
+
+	for(var item of this.items){
+
 		var itemName = item.itemName;
 		//console.log(item.top + ":----------------------:" + videoObj.position().top);
-		var top = item.top + videoObj.position().top;
-		var left = item.left + videoObj.position().left;
+		var top = item.top;
+		var left = item.left;
 		var width = item.width;
 		var height = item.height;
 		var startTime = item.startTime;
@@ -90,40 +116,124 @@ function createItemObjects(id,items,videoObj){
 		itemObj.data("left",left);
 		itemObj.data("width",width);
 		itemObj.data("height",height);
-		itemObj.data("vicId",id);
-		
+
+        ////オブジェクトをクリックした時の挙動
+        itemObj.on('click', function (evt) {
+
+            that.movieStop();
+            that.openModal(item);
+
+        });
+
 		if(typeof item.tag !== "undefined"){
-			var div = $("<div>").css(item.tag.css)
+
+			var div = $("<div>").css(item.tag.css);
+
 			if(item.tag.text){
 				div.append($("<p>" + item.tag.text + "</p>"));
 			}
+
 			itemObj.append(div);
+
 		}
 
-		itemObjects.push(itemObj);
+        item.$dom = itemObj;
+
 	}
 
-	return itemObjects;
-}
+};
 
 
-function putItem(itemObj,view){
-	var href = $("<a>",{"href":itemObj.data("url"),"target":"_blank","onClick":"movieStop(" + itemObj.data("vicId") +");"});
-	href.append(itemObj);
-	view.append(href);
-}
+Vic.prototype.putItem = function (itemObj) {
+
+	this.$video_wrap.append(itemObj);
+
+};
 
 
-function movieStop(id){
-	vics[id].pause();
-}
+Vic.prototype.movieStop = function () {
+	this.pause();
+};
 
-function moveItem(itemObj){
+Vic.prototype.moveItem = function (itemObj) {
 	if(itemObj.data("added")){
 		itemObj.data("left",itemObj.data("left") + itemObj.data("distanceByFrameX"));
 		itemObj.data("top",itemObj.data("top") + itemObj.data("distanceByFrameY"));
 		itemObj.css({"left":itemObj.data("left")});
 		itemObj.css({"top":itemObj.data("top")});
 	}
-	//console.log(itemObj.data("left"));
+};
+
+Vic.prototype.openModal = function (item) {
+
+    var target_obj = this;
+    var $dom_modal = $("<div class='modal'>" + item.details.name + "<br><button class='add_item'>リストに追加する</button><button class='close'>close</button></div>");
+    var $parent = this.$video_wrap;
+
+    $dom_modal.find('.close').on('click', function (evt) {
+        $dom_modal.remove();
+        target_obj.restart();
+    });
+
+    $dom_modal.find('.add_item').on('click', function (evt) {
+
+        item.selected = true;
+        target_obj.renderSelectedItems();
+
+    });
+    $parent.append($dom_modal);
+
+    return this;
+};
+
+
+Vic.prototype.renderSelectedItems = function () {
+
+    debugger;
+
+};
+
+Vic.prototype.init = function () {
+
+  var that = this;
+  ////ボタンイベントの設定
+  ////再生
+  this.$el.find('.play').each(function () {
+
+    $(this).on('click', function () {
+      that.play();
+    });
+
+  });
+
+  ////停止
+  this.$el.find('.pause').each(function () {
+
+    $(this).on('click', function () {
+      that.pause();
+    });
+
+  });
+
+  ////再開
+  this.$el.find('.pause').each(function () {
+
+    $(this).on('click', function () {
+      that.restart();
+    });
+
+  });
+
+};
+
+function initializeVic(){
+
+  for(var i=0; items.length > i; i++){
+
+    new Vic(items[i],$("#video-" + i));
+
+  }
+
 }
+
+
